d9205864947f39427a2cd3a7556870ff30693772
Jeroen Roefs <jeroen@softadvice.be>
2024-09-13 12:13:11 +0200

    to custom data

--- SoftAdvice.MFiles.LoadingExample/SoftAdvice.MFilesUIX.LoadingExample/app/dashboard.js
+++ SoftAdvice.MFiles.LoadingExample/SoftAdvice.MFilesUIX.LoadingExample/app/dashboard.js
@@ -1,20 +1,21 @@
 async function OnNewDashboard(dashboard) {
 	await dashboard.Events.Register(MFiles.Event.Started, async () => {
 
-		var id = dashboard.CustomData.id;
+		await dashboard.Events.Register(MFiles.Event.CustomDataChanged, async () => {
+			var id = dashboard.CustomData.id;
 
-		console.log("Dashboard " + id + " has started")
+			console.log("Dashboard " + id + " has started")
 
-		for (var i = 0; i < 10; i++) {
-			dashboard.ShellFrame.ShellUI.Vault.VaultExtensionMethodsOperations.RunExtensionMethod({
-				method_name: "SA.LoadingExample.VEM.Loading",
-				input: i.toString()
-			}).then((result) => {
-				console.log(id + " - " + result.output)
-			});
+			for (var i = 0; i < 10; i++) {
+				dashboard.ShellFrame.ShellUI.Vault.VaultExtensionMethodsOperations.RunExtensionMethod({
+					method_name: "SA.LoadingExample.VEM.Loading",
+					input: i.toString()
+				}).then((result) => {
+					console.log(id + " - " + result.output)
+				});
 
-		}
-
+			}
+		})
 	});
 }
 
--- SoftAdvice.MFiles.LoadingExample/SoftAdvice.MFilesUIX.LoadingExample/app/shellui.js
+++ SoftAdvice.MFiles.LoadingExample/SoftAdvice.MFilesUIX.LoadingExample/app/shellui.js
@@ -6,41 +6,46 @@ async function OnNewShellUI(shellUI) {
 	await shellUI.Events.Register(MFiles.Event.NewNormalShellFrame, OnNewNormalShellFrame);
 
 	var myTab;
+	var myDashboard;
 
 	// Handler for shell frame created event.
 	async function OnNewNormalShellFrame(shellFrame) {
 		// Add tab to right pane, when the shell frame is started.
-		var myTab;
-		var selectedItem = null;
 		await shellFrame.Events.Register(MFiles.Event.Started, OnStarted);
 		async function OnStarted() {
+			myTab = await shellFrame.RightPane.AddTab('load-tab', 'Loading', "_last", null);
+			await myTab.SetVisible(false);
+
 			shellFrame.Events.Register(MFiles.Event.NewShellListing, async (listing) => {
 				listing.Events.Register(MFiles.Event.SelectionChanged, SelectionChanged)
 			});
 		}
 
 		async function SelectionChanged(items) {
-			await RemoveTab()
+			await HideDashboard()
 			if (items.Count == 1)
-				await ShowTab()
+				await ShowDashboard()
 		}
 
-		async function ShowTab() {
-			myTab = await shellFrame.RightPane.AddTab('load-tab', 'Loading', "_last", null);
+		async function ShowDashboard() {
+			await myTab.SetVisible(true);
 
 			var guid = self.crypto.randomUUID();
 
 			console.log('opening dashboard ' + guid)
 
-			await myTab.ShowDashboard('loading-dashboard', { id: guid })
+			var input = { id: guid }
+
+			if (myDashboard)
+				await myDashboard.UpdateCustomData(input)
+			else
+				myDashboard = await myTab.ShowDashboard('loading-dashboard', input)
 
 			await myTab.Select();
 		}
 
-		async function RemoveTab() {
-
-			if (myTab)
-				await myTab.Remove();
+		async function HideDashboard() {
+			await myTab.SetVisible(false)
 		}
 	}
 
